a3339accc73ed1e7cb236a0a2248c5a342cbd4a7
HanBnrd <johann.benerradi@gmail.com>
2023-04-03 21:42:25 +0100

    Make viz object oriented

--- viz.py
+++ viz.py
@@ -1,75 +1,90 @@
 from threading import Thread
+import tkinter as tk
+import random
+import time
+
 
 CLOSE_WINDOW = False
 
+class Visualiser():
 
-def thread_func():
-    import tkinter as tk
-    import random
-    import time
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.canvas = tk.Canvas(self.root, height=500, width=500)
+        self.canvas.pack()
+        self.centers = {
+            'T3': [100, 250],
+            'T4': [400, 250],
+            'O1': [200, 350],
+            'O2': [300, 350],
+            'EDA': [250, 150]
+        }
+        self.colors = {
+            'T3': '#ff6600',
+            'T4': '#ff6600',
+            'O1': '#ff6600',
+            'O2': '#ff6600',
+            'EDA': '#0099ff'
+        }
+        self.sizes = {
+            'T3': 0.5,
+            'T4': 0.5,
+            'O1': 0.5,
+            'O2': 0.5,
+            'EDA': 0.5,
+        }
+        self.items = {}
+        for ch in self.centers:
+            items_xys = [self.centers[ch][0]-100*self.sizes[ch],
+                         self.centers[ch][1]-100*self.sizes[ch],
+                         self.centers[ch][0]+100*self.sizes[ch],
+                         self.centers[ch][1]+100*self.sizes[ch]]
+            self.items[ch] = self.canvas.create_oval(
+                *items_xys, fill=self.colors[ch], outline=self.colors[ch])
 
-    def on_close():
-        global CLOSE_WINDOW
-        CLOSE_WINDOW = True
+        for ch in self.centers:
+            self.canvas.create_text(
+                self.centers[ch], text=ch, font=('Helvetica','15','bold'))
+        self.window_closed = False
 
-    root = tk.Tk()
-    canvas = tk.Canvas(root, height=500, width=500)
-    canvas.pack()
+    def on_close(self):
+        self.window_closed = True
 
-    centers = {
-        'T3': [100, 250],
-        'T4': [400, 250],
-        'O1': [200, 350],
-        'O2': [300, 350],
-        'EDA': [250, 150]
-    }
-    sizes = {
-        'T3': random.random(),
-        'T4': random.random(),
-        'O1': random.random(),
-        'O2': random.random(),
-        'EDA': random.random(),
-    }
-    items = {}
-    for ch in centers:
-        items_xys = {}
-        items_xys[ch] = [centers[ch][0]-100*sizes[ch],
-                         centers[ch][1]-100*sizes[ch],
-                         centers[ch][0]+100*sizes[ch],
-                         centers[ch][1]+100*sizes[ch]]
-        items[ch] = canvas.create_oval(*items_xys[ch], fill="#ff3300", outline="#ff3300")
-
-    for ch in centers:
-        canvas.create_text(centers[ch], text=ch, font=('Helvetica','15','bold'))
-
-    def callback():
-        sizes = {
+    def callback(self):
+        self.sizes = {
             'T3': random.random(),
             'T4': random.random(),
             'O1': random.random(),
             'O2': random.random(),
             'EDA': random.random(),
         }
-        for ch in centers:
-            items_xys = {}
-            items_xys[ch] = [centers[ch][0]-100*sizes[ch],
-                             centers[ch][1]-100*sizes[ch],
-                             centers[ch][0]+100*sizes[ch],
-                             centers[ch][1]+100*sizes[ch]]
-            canvas.coords(items[ch], *items_xys[ch])
-            canvas.itemconfig(items[ch], fill='#ff3300', outline='#ff3300')
+        for ch in self.centers:
+            items_xys = [self.centers[ch][0]-100*self.sizes[ch],
+                         self.centers[ch][1]-100*self.sizes[ch],
+                         self.centers[ch][0]+100*self.sizes[ch],
+                         self.centers[ch][1]+100*self.sizes[ch]]
+            self.canvas.coords(self.items[ch], *items_xys)
 
-    root.protocol("WM_DELETE_WINDOW", on_close)
+    def main_loop(self):
+        print('Starting visualisation')
+        t1 = Thread(target=self.make_viz)
+        t1.start()
 
-    while True:
-        root.update_idletasks()
-        root.update()
-        if CLOSE_WINDOW is True:
-            break
-        callback()
-        time.sleep(0.1)
+    def make_viz(self):
+        # while self.hivemind.running:
+        #     if time() >= self.endtime:
+        #         break
+        while True:
+            self.root.update_idletasks()
+            self.root.update()
+            if self.window_closed is True:
+                break
+            self.callback()
+            time.sleep(0.1)
 
 
-t1 = Thread(target=thread_func)
-
-t1.start()
+if __name__ == "__main__":
+    # from hivemind import DataBorg
+    test = Visualiser()
+    test.make_viz()
