b9148393f5406e18a51b7a7ba100bcb34e384025
HanBnrd <johann.benerradi@gmail.com>
2023-04-04 11:49:59 +0100

    Use shorter EEG and EDA buffers

--- main.py
+++ main.py
@@ -59,11 +59,11 @@ class Visualiser:
         self.window_closed = True
 
     def callback(self):
-        self.sizes['T3'] = self.hivemind.eeg_buffer[0].mean()
-        self.sizes['T4'] = self.hivemind.eeg_buffer[1].mean()
-        self.sizes['O1'] = self.hivemind.eeg_buffer[2].mean()
-        self.sizes['O2'] = self.hivemind.eeg_buffer[3].mean()
-        self.sizes['EDA'] = self.hivemind.eda_buffer[0].mean()
+        self.sizes['T3'] = self.hivemind.eeg_buffer[0][-10:].mean()
+        self.sizes['T4'] = self.hivemind.eeg_buffer[1][-10:].mean()
+        self.sizes['O1'] = self.hivemind.eeg_buffer[2][-10:].mean()
+        self.sizes['O2'] = self.hivemind.eeg_buffer[3][-10:].mean()
+        self.sizes['EDA'] = self.hivemind.eda_buffer[0][-10:].mean()
         for ch in self.centers:
             items_xys = [self.centers[ch][0]-100*self.sizes[ch],
                          self.centers[ch][1]-100*self.sizes[ch],
