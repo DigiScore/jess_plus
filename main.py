01dacec79c28828d364decfadfa6586b610b4bbf
HanBnrd <johann.benerradi@gmail.com>
2023-04-06 11:21:24 +0100

    Fix main for merge with main later

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # import python modules
 import logging
-from tkinter import *
-from PIL import Image, ImageTk
 
 import config
 # import project modules
@@ -130,14 +128,6 @@ class Main(Visualiser):
         # self.hivemind = DataBorg()
         # logging.debug(f'Data dict initial values are = {self.hivemind}')
 
-        # build UI
-        self.root = Tk
-        self.images = []
-        self.canvas = Canvas(width=300, height=200)
-        self.canvas.pack()
-
-        # self.build_UI()
-
         # init the AI factory (inherits AIFactory, Listener)
         nebula = Nebula(
             speed=config.speed
@@ -153,24 +143,6 @@ class Main(Visualiser):
         # start Nebula AI Factory here after affect starts data moving
         robot1.main_loop()
         nebula.main_loop()
-        print('here')
-        # self.root.mainloop()
-
-    # def build_UI(self):
-    #     self.circleTL = self.make_circle(10, 10, 200, 100, fill='blue', alpha=0.5)
-    #     self.circleTR = self.make_circle(60, 10, 200, 100, fill='blue', alpha=0.5)
-    #     self.circleBL = self.make_circle(10, 60, 200, 100, fill='blue', alpha=0.5)
-    #     self.circleBR = self.make_circle(60, 60, 200, 100, fill='blue', alpha=0.5)
-    #
-    # def make_circle(self, x1, y1, x2, y2, **kwargs):
-    #     if 'alpha' in kwargs:
-    #         alpha = int(kwargs.pop('alpha') * 255)
-    #         fill = kwargs.pop('fill')
-    #         fill = self.root.winfo_rgb(color=fill) + (alpha,)
-    #         image = Image.new('RGBA', (x2-x1, y2-y1), fill)
-    #         self.images.append(ImageTk.PhotoImage(image))
-    #         self.canvas.create_image(x1, y1, image=self.images[-1], anchor='nw')
-    #     self.canvas.create_oval(x1, y1, x2, y2, **kwargs)
 
         # v = Visualiser()
         self.make_viz()
